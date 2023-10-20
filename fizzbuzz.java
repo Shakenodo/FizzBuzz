b253d85aa8ba0af34f4c6ae0e8f5a33b2f8a1703
Shawn <shawnodonoghue@icloud.com>
2023-10-20 22:46:18 +0200

    first commit

---
+++ fizzbuzz.java
@@ -0,0 +1,19 @@
+public class fizzbuzz {
+    public static void fizzbuz(int n) {
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        fizzbuz(100);
+    }
+}
